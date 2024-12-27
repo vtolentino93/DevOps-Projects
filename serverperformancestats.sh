9ed852ce7d8ac6ca9666ae08dd2d44890ed288a6
vtolentino <vincent.n.tolentino@gmail.com>
2024-12-26 19:04:19 -0800

    second update from ubuntu

--- serverperformancestats.sh
+++ serverperformancestats.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-echo "Hello World!"
+echo "Server Performance Stats"
+echo "========================"
+#Total CPU Usage
+get_cpu_usage() {
+    echo "Total CPU Usage:"
+    idle=$(mpstat 1 1 | awk '/Average/ {print 100 - $12}' | bc)
+    echo "CPU Usage: ${idle}%"
+    echo ""
+}
+get_cpu_usage
+
+#Total memory usage (free vs used including percentage)
+
+#Total disk usage (free vs userd including percentage)
+#du
+
+#Top 5 Process by CPU usage
+
+#Top 5 Processes by memory usage
+
+###additional
+#os version
+
+#uptime
+#uptime
+
+#load average
+
+#logged in users
+#who
+
+#failed login attempts
+
+
